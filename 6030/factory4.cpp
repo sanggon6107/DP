ff3ba1b63d6b626857df85e45d49ba99272423c6
SALCAN <68040183+sanggon6107@users.noreply.github.com>
2022-02-14 22:46:25 +0900

    Update factory4.cpp

--- 6030/factory4.cpp
+++ 6030/factory4.cpp
@@ -4,36 +4,40 @@
 #include "Singleton.hpp"
 using namespace std;
 
+// 1. 제품 생산하는 tatic 함수.
+// 2. 제품 등록 객체(제품의 코드와, 제품을 생산하는 static 함수 포인터를 생성자 인자로 받는다.)
+
+
 class Shape
 {
 public:
     virtual void Draw() { cout << "Draw Shape" << endl;}
 };
 
-class ShapeFactory
+class ShapeFactory  // 공장
 {
     MAKE_SINGLETON(ShapeFactory)
     typedef Shape* (*CREATOR)();
-    map<int, CREATOR> create_map;
+    map<int, CREATOR> create_map; // 제품 목록 (코드와 해당 제품 종류 생산하는 함수포인터)
 public:
     void Register( int type, CREATOR f )
     {
         create_map[type] = f;
     }
 
-    Shape* CreateShape(int type )
+    Shape* CreateShape(int type ) // 제품 생산
     {
         Shape* p = 0;
         auto ret = create_map.find( type );
         if ( ret == create_map.end() )
             return 0;
-        p = create_map[type]();
+        p = create_map[type](); // 제품 생산하는 함수 호출
 
         return p;
     }
 };
 
-struct RegisterShape
+struct RegisterShape // 제품 등록을 위한 객체.
 {
     RegisterShape( int type, Shape*(*f)() )
     {
@@ -51,10 +55,10 @@ class Rect : public Shape
 public:
     virtual void Draw() { cout << "Draw Rect" << endl;}
 
-    static Shape* Create() { return new Rect;}
-    static RegisterShape rs;
+    static Shape* Create() { return new Rect;}  // 제품 생산하는 static 함수.
+    static RegisterShape rs;  // 제품 등록 위한 객체.
 };
-RegisterShape Rect::rs( 1, &Rect::Create);
+RegisterShape Rect::rs( 1, &Rect::Create); // 제품 등록 위한 객체가 static 이므로 밖에서 한번 선언한다. 이 때 제품 코드와 생산 함수 포인터를 전달한다.
 
 
 // 모든 도형이 지켜야 하는 규칙을 매크로로 제공
